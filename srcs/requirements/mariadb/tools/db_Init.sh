0e4769e105b99b4ca9485bd598bfbdd0b63b9c67
amine reda saf <redasm39@gmail.com>
2023-02-24 10:12:13 -0500

    finaly everything is work

--- srcs/requirements/mariadb/tools/db_Init.sh
+++ srcs/requirements/mariadb/tools/db_Init.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 echo "CREATE DATABASE IF NOT EXISTS "${DB_NAME}";" > database.sql
-echo "CREATE USER IF NOT EXISTS "${DB_USER}" IDENTIFIED BY ${DB_PASSWORD};" >> database.sql
+echo "CREATE USER IF NOT EXISTS "${DB_USER}" IDENTIFIED BY" "'${DB_PASSWORD}';" >> database.sql
 echo "GRANT ALL PRIVILEGES ON "${DB_NAME}".* TO "${DB_USER}";" >> database.sql
-echo "ALTER USER ${DB_ROOTUSER}@'localhost' IDENTIFIED BY '${DB_ROOTPASS}';" >> database.sql
+echo "ALTER USER" "'${DB_ROOTUSER}'"@"'localhost'" "IDENTIFIED BY" "'${DB_ROOTPASS}';" >> database.sql
 echo "FLUSH PRIVILEGES;" >> database.sql
--- srcs/requirements/wordpress/config.sh
+++ srcs/requirements/wordpress/config.sh
@@ -1,25 +1,25 @@
 
 #!/bin/sh
 
-mkdir -p var/www/html
-rm -rf var/www/html/*
+# mkdir -p var/www/html
+# rm -rf var/www/html/*
 
-chmod 777 /var/www/html
-
-mkdir -p /run/php
+# chmod 777 /var/www/html
 
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
-    && chmod +x wp-cli.phar \
-    && mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --path=${WP_PATH} --allow-root
+# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+#     && chmod +x wp-cli.phar \
+#     && mv wp-cli.phar /usr/local/bin/wp
 
-wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --path=${WP_PATH} --allow-root --skip-check
-cat /var/www/html/wp-config.php
+# wp core download --path=${WP_PATH} --allow-root
 
-wp core install --path=${WP_PATH} --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=${WP_EMAIL} --skip-email --allow-root
+# cat /var/www/html/wp-config.php
+# wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --path=${WP_PATH} --allow-root --skip-check
+# cat /var/www/html/wp-config.php
 
-wp user create ${WP_ADMIN} ${WP_EMAIL} --user_pass=${WP_PASS} --role=author --allow-root --path=${WP_PATH}
+wp core install --path=/var/www/html --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_PASS} --admin_email=test@email.com --skip-email --allow-root
+
+wp user create ${WP_USER} ${WP_EMAIL} --user_pass=${WP_USERPASS} --role=author --allow-root --path=/var/www/html
 
 php-fpm7.3 --nodaemonize
